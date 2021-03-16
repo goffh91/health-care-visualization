58642ea1bf8d0ac4c08b601a2be8220076db741d
goffh91 <goffh91@naver.com>
2021-03-16 16:28:53 +0900

    change currentUrlParam variable to getter function

--- src/utils/history.js
+++ src/utils/history.js
@@ -2,11 +2,12 @@ import { createBrowserHistory } from 'history';
 
 const history = createBrowserHistory();
 
-export const currentUrlToParam = encodeURIComponent(
-    window.location.pathname
-    + window.location.search
-    + window.location.hash
-);
+export const getCurrentUrlToParam = () => 
+    encodeURIComponent(
+        window.location.pathname
+        + window.location.search
+        + window.location.hash
+    );
 
 export const forwardTo = location => {
     history.push(location);
